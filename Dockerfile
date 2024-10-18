b8bcb45f9b409fc33cb1f88fbdf64107d40a1811
Pradeep <nspradeep53@gmail.com>
2024-10-18 17:31:05 +0530

    Added docker for this applicatiojn

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+# Builder Stage
+FROM golang:1.23-alpine AS builder
+
+# Set the working directory
+WORKDIR /app
+
+# Install Air for live reloading
+RUN go install github.com/air-verse/air@latest
+
+# Copy go.mod and go.sum files
+COPY go.mod go.sum ./
+RUN go mod download
+
+# Copy the rest of your application code
+COPY . .
+
+# Final Stage
+FROM alpine:latest
+
+# Set the working directory
+WORKDIR /app
+
+# Copy only the Air binary from the builder stage
+COPY --from=builder /go/bin/air /usr/local/bin/air
+
+# Copy the application code from the builder stage
+COPY --from=builder /app /app
+
+# Run Air with the specified configuration file
+CMD ["air", "-c", ".air.toml"]
--- config/firebase.go
+++ config/firebase.go
@@ -2,8 +2,11 @@ package config
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
@@ -18,7 +21,29 @@ func InitializeFirebaseApp() (*auth.Client, *storage.Client, error) {
 		StorageBucket: bucketName,
 	}
 
-	opt := option.WithCredentialsFile("serviceAccountKey.json")
+	creds := map[string]interface{}{
+		"type":                        os.Getenv("FIREBASE_TYPE"),
+		"project_id":                  os.Getenv("FIREBASE_PROJECT_ID"),
+		"private_key_id":              os.Getenv("FIREBASE_PRIVATE_KEY_ID"),
+		"private_key":                 strings.Replace(os.Getenv("FIREBASE_PRIVATE_KEY"), "\\n", "\n", -1),
+		"client_email":                os.Getenv("FIREBASE_CLIENT_EMAIL"),
+		"client_id":                   os.Getenv("FIREBASE_CLIENT_ID"),
+		"auth_uri":                    os.Getenv("FIREBASE_AUTH_URI"),
+		"token_uri":                   os.Getenv("FIREBASE_TOKEN_URI"),
+		"auth_provider_x509_cert_url": os.Getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+		"client_x509_cert_url":        os.Getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+		"universe_domain":             os.Getenv("FIREBASE_UNIVERSE_DOMAIN"),
+	}
+
+	// Convert the creds map to a JSON byte array
+	credsJSON, err := json.Marshal(creds)
+	if err != nil {
+		log.Printf("error marshalling Firebase credentials: %v", err)
+		return nil, nil, fmt.Errorf("error marshalling Firebase credentials: %v", err)
+	}
+
+	// Use the JSON byte array with the Firebase option
+	opt := option.WithCredentialsJSON(credsJSON)
 	app, err := firebase.NewApp(ctx, config, opt)
 	if err != nil {
 		return nil, nil, fmt.Errorf("error initializing app: %v", err)
